822858eaafe5311a460aedf72390b8781b706612
JeonMinJae <ssi02014@naver.com>
2021-01-20 03:47:31 +0900

    Implement: 회원가입 구현

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,10 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useDispatch} from 'react-redux';
+import { registerUser} from '../../../actions/user_action';
 
-const RegisterPage = () => {
+const RegisterPage = (props) => {
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const onEmailHandler = e => {
+        setEmail(e.currentTarget.value);
+    }
+
+    const onNameHandler = e => {
+        setName(e.currentTarget.value);
+    }
+
+    const onPasswordHandler = e => {
+        setPassword(e.currentTarget.value);
+    }
+
+    const onConfirmPasswordHandler = e => {
+        setConfirmPassword(e.currentTarget.value);
+    }
+
+    const onSubmitHandler = e => {
+        e.preventDefault(); //버튼을 누르면 새로고침하는 것을 방지
+
+        console.log(`Email: ${email}`);
+        console.log(`Password: ${password}`);
+
+        if (password !== confirmPassword) {
+            return alert('비밀번호와 비밀번호 확인은 같이야 됩니다.');
+        }
+
+        let body = {
+            email: email,
+            name: name,
+            password: password,
+        }
+
+        dispatch(registerUser(body))
+            .then(response => {
+                if(response.payload.success) {
+                    props.history.push('/login');
+                } else {
+                    alert('Failed to sign up');
+                }
+            });
+    }
     return (
-        <div>
-            RegisterPage
-        </div>
+            <div style={{
+                display: 'flex', justifyContent: 'center', alignItems: 'center', 
+                width: '100%', height: '100vh'
+            }}>
+                <form style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+                >
+                    <label>Email</label>
+                    <input type="email" value={email} onChange={onEmailHandler}></input>
+
+                    <label>name</label>
+                    <input type="text" value={name} onChange={onNameHandler}></input>
+
+                    <label>Password</label>
+                    <input type="password" value={password} onChange={onPasswordHandler}></input>
+
+                    <label>Confirm Password</label>
+                    <input type="password" value={confirmPassword} onChange={onConfirmPasswordHandler}></input>
+                    
+                    <br />
+
+                    <button>
+                        회원 가입
+                    </button>
+                </form>
+            </div>
     );
 };
 
